ced61bf5bb47c71e710fcfdc20ebfa5d6e361929
20171105123 <s20171105123@admindeimac-34.local>
2018-07-05 20:21:16 +0800

    Initial Commit

---
+++ test7.5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test7.5
+//
+//  Created by 鲁祥 on 2018/7/5.
+//  Copyright © 2018年 鲁祥. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
